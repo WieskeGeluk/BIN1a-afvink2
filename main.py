cb348a7d3e754bb48c30b2477355d5d219ce5301
WieskeGeluk <89648252+WieskeGeluk@users.noreply.github.com>
2021-09-07 13:40:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Setting the path to the fasta files and setting the files to a variable
+arabidopsis_gene = open('ara_gene.txt' ,"r")
+arabidopsis_rna = open('ara_rna.txt' , "r")
+arabidopsis_thaliana_gene = arabidopsis_gene.read()
+print(arabidopsis_thaliana_gene)
+
+
